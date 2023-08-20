6c402eca2d237d844b25ad58ad3490dc3001c357
alfred kimata <alfyjason@gmail.com>
2023-08-20 08:44:18 +0300

    basic routing completed

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+    def index
+      students = Student.all
+      render json: students
+    end
+  
+    def grades
+      students_ordered_by_grade = Student.order(grade: :desc)
+      render json: students_ordered_by_grade
+    end
+  
+    def highest_grade
+      highest_grade_student = Student.order(grade: :desc).first
+      render json: highest_grade_student
+    end
+  end
+  
